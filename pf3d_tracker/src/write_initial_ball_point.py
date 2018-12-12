0628bd0833fe2c2a84e1e3d125210ae09b99d1e0
João Avelino <joao.manuel.avelino@gmail.com>
2018-12-12 20:04:12 +0000

    Python point generator now working

--- pf3d_tracker/src/write_initial_ball_point.py
+++ pf3d_tracker/src/write_initial_ball_point.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 import sys
 
-R = 198/(2*math.pi) #ball radius.
+R = 28 #ball radius.
 R1=(1-0.20)*R #ray for placing the internal points [mm]
 R2=(1+0.20)*R #ray for placing the external points [mm]
 nPoints=50
@@ -18,8 +18,8 @@ p2=R2*np.vstack((x, y, z))
 
 points = np.hstack((p1, p2))
 
-file = open("initial_ball_points_56mm_30percent.csv", "w+")
+file = open("initial_ball_points_smiley_28mm_20percent.csv", "w+")
 
 for a in range(0, 3):
     for b in range(0, nPoints*2):
-        file.write(str(points[a,b])+"\n")
+        file.write("%.6f\n" % (points[a, b]))
